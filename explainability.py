7ed0ea7e0bbb00e40a7a8023ebafcfd2c40a1144
Rahul Golder <65487329+Goldenprince8420@users.noreply.github.com>
2023-03-10 13:36:20 +0530

    Update explainability.py

--- explainability.py
+++ explainability.py
@@ -5,7 +5,7 @@ import lightgbm as lgb
 from utils import model_evaluate
 
 
-def lightgbm_model():
+def lightgbm_model_final(X_train, Y_train, X_test, Y_test):
     clf_lgbm = lgb.LGBMClassifier(
         boosting_type='dart',
         objective="multiclass",
@@ -28,27 +28,27 @@ def lightgbm_model():
     model_evaluate(Y_test, Y_pred, set_name="Test")
     return clf_lgbm
 
+if __name__ == "__main__":
+    idx = 15
+    path = "data"
+    data = get_data(path)
+    # train, test = prepare_data(data)
+    # X_train, Y_train = train
+    # X_test, Y_test = test
+    (X_train, Y_train), (X_test, Y_test) = get_partitioned_data(data)
+    explainer = lime_tabular.LimeTabularExplainer(X_train.values,
+                                                feature_names=X_train.columns,
+                                                class_names=[0, 1, 2, 3, 4], kernel_width=3)
 
-idx = 15
-path = "data"
-data = get_data(path)
-# train, test = prepare_data(data)
-# X_train, Y_train = train
-# X_test, Y_test = test
-(X_train, Y_train), (X_test, Y_test) = get_partitioned_data(data)
-explainer = lime_tabular.LimeTabularExplainer(X_train.values,
-                                              feature_names=X_train.columns,
-                                              class_names=[0, 1, 2, 3, 4], kernel_width=3)
-
-clf = lightgbm_model()
-predict_fn = lambda x: clf.predict_proba(x).astype(float)
-# Choose a local instance
-chosen = X_test.iloc[idx]
-# print(chosen)
-exp = explainer.explain_instance(chosen, predict_fn, num_features=5)
-exp.save_to_file(file_path="./data/results/explanation.html")
+    clf = lightgbm_model_final(X_train, Y_train, X_test, Y_test)
+    predict_fn = lambda x: clf.predict_proba(x).astype(float)
+    # Choose a local instance
+    chosen = X_test.iloc[idx]
+    # print(chosen)
+    exp = explainer.explain_instance(chosen, predict_fn, num_features=5)
+    exp.save_to_file(file_path="./data/results/explanation.html")
 
 
-shap_values = shap.TreeExplainer(clf).shap_values(X_test)
-shap.summary_plot(shap_values, X_test)
-shap.dependence_plot("T", shap_values[0], X_test)
+    shap_values = shap.TreeExplainer(clf).shap_values(X_test)
+    shap.summary_plot(shap_values, X_test)
+    shap.dependence_plot("T", shap_values[0], X_test)
